4f25c02cc2a90ed800869c9e4e8c3b1608146d6c
AMPortugal <adrianoc.martins@gmail.com>
2024-07-05 10:23:05 -0300

    Updated ProjectMod with new files

---
+++ src/components/g-appBar/GAppBarThemeSwitch.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <VBtn
+            v-if="currentTheme === 'light'"
+            class="mr-5"
+            density="comfortable"
+            color="g-orange-1"
+            icon="$mdiWeatherNight"
+            variant="tonal"
+            @click="currentTheme = 'dark'"
+        />
+        <VBtn
+            v-else
+            class="mr-5"
+            density="comfortable"
+            color="g-orange-1"
+            icon="$mdiWhiteBalanceSunny"
+            variant="tonal"
+            @click="currentTheme = 'light'"
+        />
+    </div>
+</template>
+<script setup>
+// Theme logic
+const { currentTheme } = toRefs(useAppStore());
+</script>
---
+++ src/components/g-appBar/GAppSearch.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-container>
+        <v-row justify="center">
+            <v-col cols="12" md="6">
+                <v-text-field
+                    v-if="showSearch"
+                    v-model="search"
+                    density="comfortable"
+                    color="g-orange-1"
+                    label="Search"
+                    class="mx-auto pa-1"
+                    max-width="450"
+                    clearable
+                    width="450"
+                    variant="underlined"
+                    hide-details
+                    single-line
+                    @keyup.enter="performSearch"
+                    @blur="toggleSearch"
+                >
+                </v-text-field>
+                <v-btn
+                    v-else
+                    density="comfortable"
+                    color="g-orange-1"
+                    variant="tonal"
+                    icon="$mdiMagnify"
+                    @click="toggleSearch"
+                >
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const showSearch = ref(false);
+const search = ref('');
+
+const toggleSearch = () => {
+    showSearch.value = !showSearch.value;
+};
+
+const performSearch = () => {
+    console.log('Performing search for:', search.value);
+    toggleSearch();
+};
+</script>
